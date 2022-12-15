0857043b8debdfaf8a82ef3e7432c48603396e40
Juseok Oh <111451710+hama-jsoh@users.noreply.github.com>
2022-12-15 11:26:21 +0900

    :memo: Add download script
    - UPT_Dataset.zip
    - model checkpoint file

---
+++ downloads.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+download_google_drive() {
+
+	wget --load-cookies ~/cookies.txt \
+	  "https://docs.google.com/uc?export=download&confirm=\
+	  $(wget --quiet --save-cookies ~/cookies.txt --keep-session-cookies --no-check-certificate \
+	  'https://docs.google.com/uc?export=download&id=$1' \
+	  -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$1" \
+	  -O $2 \
+	  && rm -rf ~/cookies.txt
+
+}
+
+main() {
+
+	download_google_drive 1QgIQJ83FXE9XLUhKdY1RK-cHr5PGAa8V UPT_512_320.zip
+  
+	download_google_drive 1k5QTVzd1B67--Y7WGejbRVA1Cgg6Wy2P network-snapshot-004408.pkl
+  
+}
+
+main
